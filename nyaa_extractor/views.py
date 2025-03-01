f88c88e4c1d4e9c132f03fa6f9c6f22f3eb78208
YKGio <aresr417@gmail.com>
2025-03-01 23:21:16 +0800

    新增支持分頁查詢的功能，更新搜尋 URL 以包含分頁參數

--- nyaa_extractor/views.py
+++ nyaa_extractor/views.py
@@ -7,7 +7,11 @@ def ui(request):
 
 def get_magnets(request):
     query = request.GET.get('query')
+    p = request.GET.get('p')
     if query:
+        if p :
+            query = query + "&p=" + p
+        print(f"URL: https://sukebei.nyaa.si/?f=0&c=0_0&q={query}")
         search_url = f"https://sukebei.nyaa.si/?f=0&c=0_0&q={query}"
         magnets = fetch_magnet.call(search_url)
         return JsonResponse({"magnets": magnets})
