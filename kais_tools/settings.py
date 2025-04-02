745d905f003d4cbdd541034b1866889624369a20
YKGio <aresr417@gmail.com>
2025-04-02 08:02:44 +0800

    更新設定以禁用調試模式，並使用 dj_database_url 配置資料庫連接；新增 build.sh 腳本以自動化部署流程

--- kais_tools/settings.py
+++ kais_tools/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,11 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fk+k9ul%6g&@r&sbsziieaqdsfa1)o$3!z^b%m*5e5#4j5^48j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'kais-tools.onrender.com',
-    'localhost',
 ]
 
 # Application definition
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'kais_tools.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@ldpg-cv1ko4pu0jms738j0sc0-a:5432',
+        conn_max_age=600
+        )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
